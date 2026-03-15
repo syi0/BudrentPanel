c71c458faf150133f6978f86b13a3fd71dd1d904
syi0 <syi0.v006@gmail.com>
2026-03-15 20:52:53 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -125,7 +125,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
